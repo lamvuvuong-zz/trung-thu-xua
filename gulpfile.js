2e6e4ea937c2f4d66c76bc3a647ad42b3bdccb70
vuonglam_nau <vuong.lam@naustud.io>
2017-08-30 09:59:00 +0700

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,24 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var livereload = require('gulp-livereload');
 var minifyCss = require('gulp-cssnano');
 
 // Compile Our Sass
 gulp.task('sass', function () {
-	return gulp.src('public/*.scss')
+	return gulp.src('public/stylesheets/*.*')
 		.pipe(sass())
 		.pipe(minifyCss())
-		.pipe(gulp.dest('public'))
+		.pipe(gulp.dest('public/stylesheets'))
 		.pipe(livereload());
 });
 
 // Watch Files For Changes
 gulp.task('watch', function () {
 	livereload.listen();
-	gulp.watch(['public/**/*'], ['sass']);
+	gulp.watch(['public/stylesheets/**/*'], ['sass']);
 });
 
 // Default Task
